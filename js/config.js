e82d4354d927ca2544f9e6644df0104d09b2ecdd
ss <ss@local>
2026-08-20 14:04:54 +0530

    Updated details.

--- js/config.js
+++ js/config.js
@@ -18,8 +18,8 @@
 */
 
 const CONFIG = {
-  JOBS_CSV_URL: "https://docs.google.com/spreadsheets/d/1r-dur5Ndz7CXm41aB__lIAcuJcrQniRHPjK6OqSfysA/edit?gid=2101256547#gid=2101256547",    // <-- paste the published "Job Openings" CSV link here
-  PROFILE_CSV_URL: "https://docs.google.com/spreadsheets/d/1r-dur5Ndz7CXm41aB__lIAcuJcrQniRHPjK6OqSfysA/edit?gid=1893347345#gid=1893347345", // <-- paste the published "Recruiter Profile" CSV link here
+  JOBS_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRVWK0Y9FxMB4b4o1OMCiImLstCQFhbpb84fW4lmnu2HrQ8FG4kmYegv53ULeslsjV4R3VB7CDYpRwZ/pub?gid=2101256547&single=true&output=csv",    // <-- paste the published "Job Openings" CSV link here
+  PROFILE_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRVWK0Y9FxMB4b4o1OMCiImLstCQFhbpb84fW4lmnu2HrQ8FG4kmYegv53ULeslsjV4R3VB7CDYpRwZ/pub?gid=1893347345&single=true&output=csv", // <-- paste the published "Recruiter Profile" CSV link here
 
   // Local fallback data, used only when the URLs above are empty (for local preview/testing).
   FALLBACK_JOBS_CSV: "data/sample-jobs.csv",
